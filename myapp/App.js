545a83451e8c3dac461a92725078acb3422ee228
Jai Narayana <jai.sridharanmurali@ucdconnect.ie>
2023-08-23 11:43:51 +0100

    login first check
    BASIC SETUP WITH NATIBE BASE

--- myapp/App.js
+++ myapp/App.js
@@ -1,53 +1,34 @@
 import React from "react";
 import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TextInput,
-  TouchableOpacity,
+  StatusBar
 } from "react-native";
-import { StatusBar } from 'react-native';
 import { registerRootComponent } from 'expo';
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack"; // Import createStackNavigator
-import login from "./Login"; // Import the Login component
+import { SafeAreaProvider } from 'react-native-safe-area-context'; // Import SafeAreaProvider
+import { enableScreens } from 'react-native-screens'; // Import enableScreens
+import Login  from "./Login"; // Import the Login component
+import {NativeBaseProvider} from 'native-base';
 
+enableScreens(); 
 // Create a Stack Navigator
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
+    <NativeBaseProvider >
     <NavigationContainer>
-      <StatusBar barStyle="default" /> 
-      <Stack.Navigator
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: "black",
-            height: 40 // Set the background color to black
-          },
-          headerTintColor: "white",
-          headerTitleAlign: 'center',
-          headerTitleStyle: {   // Set the font style for the header title
-            fontSize: 25, // Set the font size
-            fontWeight: 'bold', // Set the font weight
-          } // Set the text color to white
-        }}
-      >
-        <Stack.Screen name="Login" component={login} />
-      </Stack.Navigator>
+        <StatusBar barStyle="default" /> 
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login } 
+             options={{
+              headerShown: false, // Hide the header
+            }}
+        />
+        </Stack.Navigator>
     </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
 
 registerRootComponent(App);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: '20',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- myapp/Login.js
+++ myapp/Login.js
@@ -1,16 +1,10 @@
-
 import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TextInput,
-  TouchableOpacity,
-} from "react-native";
+import { StyleSheet, Image, View } from "react-native";
+import { Box, Button, Input, Text } from 'native-base';
+import { LinearGradient } from 'expo-linear-gradient';
+import logo from './assets/logo.png';
 
 const Login = () => {
-
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
 
@@ -20,67 +14,88 @@ const Login = () => {
     console.log("Email:", email);
   };
 
-
   return (
-    <View style={styles.container}>
-    <View>
-      <Text style={{ marginBottom: 50 , textAlign: "center"}}>Form Example</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Name"
-        value={name}
-        onChangeText={(text) => setName(text)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-      />
-    </View>
-    <View>
-      <TouchableOpacity style={styles.button} onPress={handleSubmit}>
-        <Text style={styles.buttonText}>Submit</Text>
-      </TouchableOpacity>
-    </View>
-  </View>
-    
+    <LinearGradient
+      colors={['#1a1a1a', '#210606']}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+      style={styles.container}
+    >
+      <Image source={logo} style={styles.logo} />
+      <View style={styles.form}>
+        <Input
+          placeholder="Name"
+          value={name}
+          placeholderTextColor="white"
+          onChangeText={(text) => setName(text)}
+          style={styles.input}
+        />
+        <Input
+          placeholder="Email"
+          value={email}
+          placeholderTextColor="white"
+          onChangeText={(text) => setEmail(text)}
+          style={styles.input}
+        />
+        <Button
+          onPress={handleSubmit}
+          style={styles.button}
+          colorScheme="danger"
+        >
+          Submit
+        </Button>
+        <Button onPress={() => console.log("Button pressed")}>Press Me</Button>
+      </View>
+    </LinearGradient>
   );
 };
 
 export default Login;
+// ...rest of your styles and exports
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 0, // Add your desired padding value here
-    borderWidth: 5,
-    borderColor: "#ccc",
-    borderRadius: 10,
+    overflow: 'hidden'
+    
   },
+
+  logo: {
+    marginTop: '50%',
+    marginBottom: '20%',
+    alignSelf: 'center',
+    height: '18%',
+    aspectRatio: 1, 
+  },
+
+  form: {
+    marginHorizontal: 40
+  },
+
   input: {
     width: "100%",
+    color: "white",
     height: 40,
     borderWidth: 1,
     borderColor: "#ccc",
     borderRadius: 8,
-    paddingHorizontal: 50,
+    paddingHorizontal: 10,
     marginBottom: 10,
     textAlign: "left",
   },
+
   button: {
     backgroundColor: "#c83f3f",
     borderRadius: 8,
     paddingVertical: 10,
-    paddingHorizontal: 90,
+    paddingHorizontal: 10,
+    marginHorizontal: 80,
     marginTop: 40,
+    marginBottom: 80
   },
+
   buttonText: {
     color: "white",
     textAlign: "center",
   },
 });
-
-
